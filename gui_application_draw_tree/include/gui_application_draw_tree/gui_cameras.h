039bc825fcdbb6d23e495f420a351a890f317e37
xaedes <xaedes@gmail.com>
2021-11-05 15:29:44 +0100

    refactored GuiCameras and added orthogonal camera

--- gui_application_draw_tree/include/gui_application_draw_tree/gui_cameras.h
+++ gui_application_draw_tree/include/gui_application_draw_tree/gui_cameras.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <glm/glm.hpp>
+#include <type_traits>
 
 #include "gl_classes/imgui_gl.h"
 #include "draw_tree_gl/drawable/drawable.h"
@@ -23,35 +24,95 @@ namespace gui_application_draw_tree {
         using FirstPersonView              = cameras_gl::FirstPersonView;
         using LookAtView                   = cameras_gl::LookAtView;
         using OrbitalView                  = cameras_gl::OrbitalView;
+        
+        using PerspectiveProjection         = cameras_gl::PerspectiveProjection;
+        using SymmetricOrthogonalProjection = cameras_gl::SymmetricOrthogonalProjection;
+
         using FirstPersonPerspectiveCamera = cameras_gl::FirstPersonPerspectiveCamera;
         using OrbitalPerspectiveCamera     = cameras_gl::OrbitalPerspectiveCamera;
         using LookAtPerspectiveCamera      = cameras_gl::LookAtPerspectiveCamera;
-        using PerspectiveProjection        = cameras_gl::PerspectiveProjection;
+
+        using FirstPersonSymmetricOrthogonalCamera  = cameras_gl::FirstPersonSymmetricOrthogonalCamera;
+        using LookAtSymmetricOrthogonalCamera       = cameras_gl::LookAtSymmetricOrthogonalCamera;
+        using OrbitalSymmetricOrthogonalCamera      = cameras_gl::OrbitalSymmetricOrthogonalCamera;
+
 
         GuiCameras(Application& app);
         virtual ~GuiCameras();
         void setup();
         void frame();
 
-        CameraInterface* activeCamera() { return m_activeCamera; }
+        CameraInterface* activeCamera() { return getCamera(activeCameraIndex()); }
         glm::vec2 cursor() { return m_cursor; };
 
         const glm::mat4& projection() const { return m_projection; }
         const glm::mat4& view() const { return m_view; }
         const glm::mat4& projection_view() const { return m_projection_view; }
-    protected:
 
-        void updateInput(CameraInterface&, FirstPersonView& fpv_view);
-        void updateInput(CameraInterface&, LookAtView& lookat_view);
-        void updateInput(CameraInterface&, OrbitalView& orbital_view);
+    protected:
+        void update(CameraInterface&, PerspectiveProjection* projection);
+        void update(CameraInterface&, SymmetricOrthogonalProjection* projection);
+
+        void update(CameraInterface&, FirstPersonView* view);
+        void update(CameraInterface&, LookAtView* view);
+        void update(CameraInterface&, OrbitalView* view);
         
+    protected:
+        void update(CameraInterface&, PerspectiveProjection& projection);
+        void update(CameraInterface&, SymmetricOrthogonalProjection& projection);
+
+        void update(CameraInterface&, FirstPersonView& view);
+        void update(CameraInterface&, LookAtView& view);
+        void update(CameraInterface&, OrbitalView& view);
+        
+    protected:
+        void updateFromImGui(CameraInterface&, PerspectiveProjection& projection);
+        void updateFromImGui(CameraInterface&, SymmetricOrthogonalProjection& projection);
+
+        void updateFromImGui(CameraInterface&, FirstPersonView& view);
+        void updateFromImGui(CameraInterface&, LookAtView& view);
+        void updateFromImGui(CameraInterface&, OrbitalView& view);
+        
+    protected:
+        void updateFromInput(CameraInterface&, PerspectiveProjection& projection);
+        void updateFromInput(CameraInterface&, SymmetricOrthogonalProjection& projection);
+        
+        void updateFromInput(CameraInterface&, FirstPersonView& view);
+        void updateFromInput(CameraInterface&, LookAtView& view);
+        void updateFromInput(CameraInterface&, OrbitalView& view);
+        
+        bool SliderFloatWithUnit(const char* name, float* ptr, float min, float max, float ui_unit);
+
         Application& app;
 
-        FirstPersonPerspectiveCamera m_firstPersonCamera;
-        OrbitalPerspectiveCamera m_orbitalCamera;
-        LookAtPerspectiveCamera m_lookAtCamera;
+        FirstPersonPerspectiveCamera         m_firstPersonPerspectiveCamera;
+        OrbitalPerspectiveCamera             m_orbitalPerspectiveCamera;
+        LookAtPerspectiveCamera              m_lookAtPerspectiveCamera;
+
+        FirstPersonSymmetricOrthogonalCamera m_firstSymmetricOrthogonalPersonCamera;
+        LookAtSymmetricOrthogonalCamera      m_lookAtSymmetricOrthogonalCamera;
+        OrbitalSymmetricOrthogonalCamera     m_orbitalSymmetricOrthogonalCamera;
+
+        PerspectiveProjection         m_perspectiveProjection;
+        SymmetricOrthogonalProjection m_symmetricOrthogonalProjection;
+
+        FirstPersonView m_firstPersonView;
+        LookAtView      m_lookAtView;
+        OrbitalView     m_orbitalView;
+
+        using NumProjection = std::integral_constant<uint32_t, 2>;
+        using NumView = std::integral_constant<uint32_t, 3>;
         
-        CameraInterface* m_activeCamera = nullptr;
+        CameraInterface* getCamera(int index);
+
+        uint32_t activeCameraIndex() const { return m_activeProjection*NumView::value+m_activeView; }
+
+        uint32_t m_activeProjection = 0;
+        uint32_t m_activeView = 0;
+
+        float D2R = 3.1415926535897932384626433832795f / 180.0f;
+        float R2D = 180.0f / 3.1415926535897932384626433832795f;
+
         glm::mat4 m_projection;
         glm::mat4 m_view;
         glm::mat4 m_projection_view;
--- gui_application_draw_tree/src/gui_cameras.cpp
+++ gui_application_draw_tree/src/gui_cameras.cpp
@@ -20,129 +20,273 @@ namespace gui_application_draw_tree {
 
     void GuiCameras::setup()
     {
-        m_activeCamera = &static_cast<CameraInterface&>(m_orbitalCamera);
-        m_orbitalCamera.accessView().setDistance(36);
+        m_activeProjection = 0;
+        m_activeView = 1;
+        // m_activeCamera = &static_cast<CameraInterface&>(m_orbitalCamera);
+        m_orbitalView.setDistance(36);
+        m_orbitalView.setDistance(6000); 
+        m_perspectiveProjection.setFarPlane(10000);
+        m_symmetricOrthogonalProjection.setFarPlane(10000);
+        m_symmetricOrthogonalProjection.setWidth(3000);
     }
 
     void GuiCameras::frame()
     {
         int display_w, display_h;
         glfwGetFramebufferSize(app.window(), &display_w, &display_h);
-        // auto* vp = ImGui::GetMainViewport();
-        // vp->WorkPos.x
-    // WorkSize    
         auto cursor_pos = ImGui::GetMousePos();
-        // auto win_pos = ImGui::GetWindowPos();
         m_cursor = glm::vec2(cursor_pos.x , (display_h-1) - cursor_pos.y);
         ImGui::Begin("Camera");
-
-        PerspectiveProjection* projection = dynamic_cast<PerspectiveProjection*>(&m_activeCamera->projection());
-        if (projection)
         {
-            float fov_deg = projection->fov() * 180 / 3.14159 ;
-            if (ImGui::SliderFloat("fov", &fov_deg, 0, 180))
+            ImGui::BeginTabBar("Projection Options");
             {
-                projection->setFov(fov_deg * 3.14159 / 180);
+                if (ImGui::BeginTabItem("Perspective Projection"))
+                {
+                    m_activeProjection = 0;
+                    update(*activeCamera(), dynamic_cast<PerspectiveProjection*>(&activeCamera()->projection()));
+                    ImGui::EndTabItem();
+                }
+                if (ImGui::BeginTabItem("Orthogonal Projection"))
+                {
+                    m_activeProjection = 1;
+                    update(*activeCamera(), dynamic_cast<SymmetricOrthogonalProjection*>(&activeCamera()->projection()));
+                    ImGui::EndTabItem();
+                }
             }
-            bool changed = false;
-            changed |= ImGui::SliderFloat("near", &projection->accessNearPlane(), 0, 100);
-            changed |= ImGui::SliderFloat("far", &projection->accessFarPlane(), 0, 10000);
-            if (changed)
-            {
-                projection->update();
-            }
+            ImGui::EndTabBar();
 
-        }
-        
-        ImGui::BeginTabBar("Camera Options");
-
-        if (ImGui::BeginTabItem("Orbital Camera"))
-        {
-            bool changed = false;
-            auto& view = m_orbitalCamera.accessView();
-            changed |= ImGui::SliderFloat("target.x", &view.accessTarget().x, -150, 150);
-            changed |= ImGui::SliderFloat("target.y", &view.accessTarget().y, -150, 150);
-            changed |= ImGui::SliderFloat("target.z", &view.accessTarget().z, -150, 150);
-            changed |= ImGui::SliderFloat("roll",  &view.accessAngles().x, -3.14159 * 2, 3.14159 * 2);
-            changed |= ImGui::SliderFloat("pitch", &view.accessAngles().y, -3.14159 * 2, 3.14159 * 2);
-            changed |= ImGui::SliderFloat("yaw",   &view.accessAngles().z, -3.14159 * 2, 3.14159 * 2);
-            static float max_distance = 150;
-            changed |= ImGui::SliderFloat("distance", &view.accessDistance(), 0, max_distance);
-            changed |= ImGui::Checkbox("swap y & z", &view.accessSwapYZ());
-            if (view.distance() >= max_distance)
+            ImGui::BeginTabBar("View Options");
             {
-                max_distance = view.distance() + 20;
+                if (ImGui::BeginTabItem("Orbital View"))
+                {
+                    m_activeView = 1;
+                    update(*activeCamera(), dynamic_cast<OrbitalView*>(&activeCamera()->view()));
+                    ImGui::EndTabItem();
+                }
+                if (ImGui::BeginTabItem("First Person View"))
+                {
+                    m_activeView = 0;
+                    update(*activeCamera(), dynamic_cast<FirstPersonView*>(&activeCamera()->view()));
+                    ImGui::EndTabItem();
+                }
+                if (ImGui::BeginTabItem("Look At  View"))
+                {
+                    m_activeView = 2;
+                    update(*activeCamera(), dynamic_cast<LookAtView*>(&activeCamera()->view()));
+                    ImGui::EndTabItem();
+                }
             }
-            if (changed)
-            {
-                view.update();
-            }
-            ImGui::EndTabItem();
-            m_activeCamera = &static_cast<CameraInterface&>(m_orbitalCamera);
-
-            updateInput(*m_activeCamera, view);
+            ImGui::EndTabBar();
         }
-        if (ImGui::BeginTabItem("First Person Camera"))
-        {
-            bool changed = false;
-            auto& view = m_firstPersonCamera.accessView();
-            changed |= ImGui::SliderFloat("position.x", &view.accessPosition().x, -100, 100);
-            changed |= ImGui::SliderFloat("position.y", &view.accessPosition().y, -100, 100);
-            changed |= ImGui::SliderFloat("position.z", &view.accessPosition().z, -100, 100);
-            changed |= ImGui::SliderFloat("roll",  &view.accessRollPitchYaw().x, -3.14159 * 2, 3.14159 * 2);
-            changed |= ImGui::SliderFloat("pitch", &view.accessRollPitchYaw().y, -3.14159 * 2, 3.14159 * 2);
-            changed |= ImGui::SliderFloat("yaw",   &view.accessRollPitchYaw().z, -3.14159 * 2, 3.14159 * 2);
-            if (changed)
-            {
-                view.update();
-            }
-            ImGui::EndTabItem();
-            m_activeCamera = &static_cast<CameraInterface&>(m_firstPersonCamera);
-            updateInput(*m_activeCamera, view);
-        }
-        if (ImGui::BeginTabItem("Look At Camera"))
-        {
-            bool changed = false;
-            auto& view = m_lookAtCamera.accessView();
-            changed |= ImGui::SliderFloat("position.x", &view.accessPosition().x, -100, 100);
-            changed |= ImGui::SliderFloat("position.y", &view.accessPosition().y, -100, 100);
-            changed |= ImGui::SliderFloat("position.z", &view.accessPosition().z, -100, 100);
-            changed |= ImGui::SliderFloat("target.x", &view.accessTarget().x, -100, 100);
-            changed |= ImGui::SliderFloat("target.y", &view.accessTarget().y, -100, 100);
-            changed |= ImGui::SliderFloat("target.z", &view.accessTarget().z, -100, 100);
-            if (changed)
-            {
-                view.update();
-            }
-            ImGui::EndTabItem();
-            m_activeCamera = &static_cast<CameraInterface&>(m_lookAtCamera);
-            updateInput(*m_activeCamera, view);
-        }
-        ImGui::EndTabBar();
-
         ImGui::End();
 
-        if (m_activeCamera)
+        if (activeCamera())
         {
-            m_activeCamera->projection().setViewportSize({ display_w, display_h });
-            m_projection = m_activeCamera->projection().mat();
-            m_view = m_activeCamera->view().mat();
+            auto cam = activeCamera();
+            cam->projection().setViewportSize({ display_w, display_h });
+            m_projection = cam->projection().mat();
+            m_view = cam->view().mat();
             m_projection_view = m_projection * m_view;
         }
-
     }
 
-    void GuiCameras::updateInput(CameraInterface& cam, FirstPersonView& fpv_view)
+    GuiCameras::CameraInterface* GuiCameras::getCamera(int index)
+    {
+        switch(index)
+        {
+        case 0: return static_cast<CameraInterface*>(&m_firstPersonPerspectiveCamera);         break;
+        case 1: return static_cast<CameraInterface*>(&m_orbitalPerspectiveCamera);             break;
+        case 2: return static_cast<CameraInterface*>(&m_lookAtPerspectiveCamera);              break;
+        case 3: return static_cast<CameraInterface*>(&m_firstSymmetricOrthogonalPersonCamera); break;
+        case 4: return static_cast<CameraInterface*>(&m_orbitalSymmetricOrthogonalCamera);     break;
+        case 5: return static_cast<CameraInterface*>(&m_lookAtSymmetricOrthogonalCamera);      break;
+        default: return nullptr;                                                               break;
+        }
+    }
+
+    void GuiCameras::update(CameraInterface& cam, PerspectiveProjection* projection)
+    {
+        if (projection != nullptr) update(cam, *projection);
+    }
+
+    void GuiCameras::update(CameraInterface& cam, SymmetricOrthogonalProjection* projection)
+    {
+        if (projection != nullptr) update(cam, *projection);
+    }
+
+    void GuiCameras::update(CameraInterface& cam, FirstPersonView* view)
+    {
+        if (view != nullptr) update(cam, *view);
+    }
+
+    void GuiCameras::update(CameraInterface& cam, LookAtView* view)
+    {
+        if (view != nullptr) update(cam, *view);
+    }
+
+    void GuiCameras::update(CameraInterface& cam, OrbitalView* view)
+    {
+        if (view != nullptr) update(cam, *view);
+    }
+
+    void GuiCameras::update(CameraInterface& cam, PerspectiveProjection& projection)
+    {
+        projection = m_perspectiveProjection;
+        updateFromImGui(cam, projection);
+        updateFromInput(cam, projection);
+        m_perspectiveProjection = projection;
+    }
+
+    void GuiCameras::update(CameraInterface& cam, SymmetricOrthogonalProjection& projection)
+    {
+        projection = m_symmetricOrthogonalProjection;
+        updateFromImGui(cam, projection);
+        updateFromInput(cam, projection);
+        m_symmetricOrthogonalProjection = projection;
+    }
+
+    void GuiCameras::update(CameraInterface& cam, FirstPersonView& view)
+    {
+        view = m_firstPersonView;
+        updateFromImGui(cam, view);
+        updateFromInput(cam, view);
+        m_firstPersonView = view;
+    }
+
+    void GuiCameras::update(CameraInterface& cam, LookAtView& view)
+    {
+        view = m_lookAtView;
+        updateFromImGui(cam, view);
+        updateFromInput(cam, view);
+        m_lookAtView = view;
+    }
+
+    void GuiCameras::update(CameraInterface& cam, OrbitalView& view)
+    {
+        view = m_orbitalView;
+        updateFromImGui(cam, view);
+        updateFromInput(cam, view);
+        m_orbitalView = view;
+    }
+
+    void GuiCameras::updateFromImGui(CameraInterface& cam, PerspectiveProjection& projection)
+    {
+        // float fov_deg = projection.fov() * 180 / 3.14159 ;
+        // if (ImGui::SliderFloat("fov", &fov_deg, 0, 180))
+        // {
+        //     projection.setFov(fov_deg * 3.14159 / 180);
+        // }
+        bool changed = false;
+        changed |= SliderFloatWithUnit("near", &projection.accessFov(), 0, 100, R2D);
+        changed |= ImGui::SliderFloat("near", &projection.accessNearPlane(), 0, 100);
+        changed |= ImGui::SliderFloat("far", &projection.accessFarPlane(), 0, 10000);
+        if (changed)
+        {
+            projection.update();
+        }
+    }
+
+    void GuiCameras::updateFromImGui(CameraInterface& cam, SymmetricOrthogonalProjection& projection)
+    {
+        bool changed = false;
+        static float max_width = 10000;
+        static float max_offset_x = 2000;
+        static float max_offset_y = 2000;
+        changed |= ImGui::SliderFloat("width", &projection.accessWidth(), 0, max_width);
+        changed |= ImGui::SliderFloat("offset_x", &(projection.accessOffset().x), -max_offset_x, max_offset_x);
+        changed |= ImGui::SliderFloat("offset_y", &(projection.accessOffset().y), -max_offset_x, max_offset_y);
+        changed |= ImGui::SliderFloat("near", &projection.accessNearPlane(), 0, 100);
+        changed |= ImGui::SliderFloat("far", &projection.accessFarPlane(), 0, 10000);
+        if (projection.width() >= max_width) max_width = projection.width() + 100;
+        if (abs(projection.offset().x) >= max_offset_x) max_offset_x = abs(projection.offset().x) + 100;
+        if (abs(projection.offset().y) >= max_offset_y) max_offset_y = abs(projection.offset().y) + 100;
+        if (changed)
+        {
+            projection.update();
+        }
+    }
+
+    void GuiCameras::updateFromImGui(CameraInterface& cam, FirstPersonView& view)
+    {
+        bool changed = false;
+        changed |= ImGui::SliderFloat("position.x", &view.accessPosition().x, -100, 100);
+        changed |= ImGui::SliderFloat("position.y", &view.accessPosition().y, -100, 100);
+        changed |= ImGui::SliderFloat("position.z", &view.accessPosition().z, -100, 100);
+        changed |= SliderFloatWithUnit("roll",  &view.accessRollPitchYaw().x, -360, 360, R2D);
+        changed |= SliderFloatWithUnit("pitch", &view.accessRollPitchYaw().y, -360, 360, R2D);
+        changed |= SliderFloatWithUnit("yaw",   &view.accessRollPitchYaw().z, -360, 360, R2D);
+        if (changed)
+        {
+            view.update();
+        }
+    }
+
+    void GuiCameras::updateFromImGui(CameraInterface& cam, LookAtView& view)
+    {
+        bool changed = false;
+        changed |= ImGui::SliderFloat("position.x", &view.accessPosition().x, -100, 100);
+        changed |= ImGui::SliderFloat("position.y", &view.accessPosition().y, -100, 100);
+        changed |= ImGui::SliderFloat("position.z", &view.accessPosition().z, -100, 100);
+        changed |= ImGui::SliderFloat("target.x", &view.accessTarget().x, -100, 100);
+        changed |= ImGui::SliderFloat("target.y", &view.accessTarget().y, -100, 100);
+        changed |= ImGui::SliderFloat("target.z", &view.accessTarget().z, -100, 100);
+        if (changed)
+        {
+            view.update();
+        }
+    }
+
+    void GuiCameras::updateFromImGui(CameraInterface& cam, OrbitalView& view)
+    {
+        bool changed = false;
+        changed |= ImGui::SliderFloat("target.x", &view.accessTarget().x, -150, 150);
+        changed |= ImGui::SliderFloat("target.y", &view.accessTarget().y, -150, 150);
+        changed |= ImGui::SliderFloat("target.z", &view.accessTarget().z, -150, 150);
+        changed |= SliderFloatWithUnit("roll",  &view.accessAngles().x, -360, 360, R2D);
+        changed |= SliderFloatWithUnit("pitch", &view.accessAngles().y, -360, 360, R2D);
+        changed |= SliderFloatWithUnit("yaw",   &view.accessAngles().z, -360, 360, R2D);
+        static float max_distance = 150;
+        changed |= ImGui::SliderFloat("distance", &view.accessDistance(), 0, max_distance);
+        changed |= ImGui::Checkbox("swap y & z", &view.accessSwapYZ());
+        if (view.distance() >= max_distance) max_distance = view.distance() + 20;
+        if (changed)
+        {
+            view.update();
+        }
+    }
+
+    bool GuiCameras::SliderFloatWithUnit(const char* name, float* ptr, float min, float max, float ui_unit)
+    {
+        float value_scaled = (*ptr) * ui_unit;
+        if (ImGui::SliderFloat(name, &value_scaled, min, max))
+        {
+            *ptr = value_scaled / ui_unit;
+            return true;
+        }
+        return false;
+    }
+
+    void GuiCameras::updateFromInput(CameraInterface& cam, PerspectiveProjection& projection)
     {
 
     }
 
-    void GuiCameras::updateInput(CameraInterface& cam, LookAtView& lookat_view)
+    void GuiCameras::updateFromInput(CameraInterface& cam, SymmetricOrthogonalProjection& projection)
     {
 
     }
 
-    void GuiCameras::updateInput(CameraInterface& cam, OrbitalView& view)
+    void GuiCameras::updateFromInput(CameraInterface& cam, FirstPersonView& view)
+    {
+
+    }
+
+    void GuiCameras::updateFromInput(CameraInterface& cam, LookAtView& view)
+    {
+
+    }
+
+    void GuiCameras::updateFromInput(CameraInterface& cam, OrbitalView& view)
     {
 
     }
